0eac26306bd1d73b9ab995be2365b5db3d6f1595
= <lewlinantony@gmail.com>
2024-11-25 18:34:44 +0530

    blank window

--- main.cpp
+++ main.cpp
@@ -1,6 +1,20 @@
 #include <iostream>
+#include <raylib.h>
+
+int windowWidth = 380;
+int windowHeight = 700;
+Color bg_colour = {0,1,45,1};
 
 int main(){
-    std::cout<<"Hello World "<<std::endl;
+    InitWindow(windowWidth,windowHeight,"Tetris");
+    SetTargetFPS(60);
+
+    while(WindowShouldClose()==false){
+        BeginDrawing();
+        ClearBackground(bg_colour);
+        EndDrawing();
+    }
+
+    CloseWindow();
     return 0;
 }
