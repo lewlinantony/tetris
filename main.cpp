a87c33145106b149588e4c9629770a9cae498ac7
= <lewlinantony@gmail.com>
2024-11-25 18:57:52 +0530

    grid visualiser

--- main.cpp
+++ main.cpp
@@ -1,17 +1,35 @@
 #include <iostream>
 #include <raylib.h>
 
-int windowWidth = 380;
+int windowWidth = 375;
 int windowHeight = 700;
-Color bg_colour = {0,1,45,1};
+int cellSize = 25;
+Color bgColour = {0,1,45,1};
+bool visualiseGrid = true;
 
 int main(){
-    InitWindow(windowWidth,windowHeight,"Tetris");
+    InitWindow(windowWidth,windowHeight,"Tetris");‸‸
     SetTargetFPS(60);
 
     while(WindowShouldClose()==false){
         BeginDrawing();
-        ClearBackground(bg_colour);
+
+        if(visualiseGrid)
+        {        
+            for(int x=0;x<windowWidth;x=x+cellSize){
+                for(int y=0;y<windowHeight;y+=cellSize){
+                    Rectangle cell = {
+                        (float)x,
+                        (float)y,
+                        (float)cellSize,
+                        (float)cellSize,   // since we are drawing a square width and height are the same i.e. cellsize
+                    };
+                    DrawRectangleLinesEx(cell,0.5,WHITE);
+                }
+            }
+        }
+
+        ClearBackground(bgColour);
         EndDrawing();
     }
 
