aec6604c96953b7c288e4c8d13b9e3be5d64377e
= <lewlinantony@gmail.com>
2024-11-27 21:22:58 +0530

    imporved code structure a it(no class yet)

--- main.cpp
+++ main.cpp
@@ -17,7 +17,7 @@ double currentTime;
 
 Color bgColour = {0,1,45,1};
 Color whiteMask = {255,255,255,100};
-bool visualiseGrid = true;
+bool visualise = true;
 
 
 
@@ -38,6 +38,50 @@ void Draw(std::vector<Rectangle>& blocks){
 
 }
 
+void visualieGrid(bool visualise){
+    if(visualise)
+    {        
+        for(int x=0;x<WINDOW_WIDTH;x=x+BLOCK_SIZE){
+            for(int y=0;y<WINDOW_HEIGHT;y+=BLOCK_SIZE){
+                Rectangle cell = {
+                    (float)x,
+                    (float)y,
+                    (float)BLOCK_SIZE,
+                    (float)BLOCK_SIZE,   // since we are drawing a square width and height are the same i.e. BLOCK_SIZE
+                };
+                DrawRectangleLinesEx(cell,0.5,whiteMask);
+            }
+        }
+    }
+}
+
+void updateBlocksTop(std::unordered_map<float,float>& top,std::vector<Rectangle>& blocks){
+        for(Rectangle& block:blocks){
+            if(((block.y+25)<=(top[block.x]))){
+                block.y+=25;
+            } 
+            else{
+                top[block.x]=block.y-25;                
+            }
+        }
+}
+
+void spawnBlock(std::vector<Rectangle>& blocks){
+    Rectangle tempBlock;
+    Vector2 mousePosition = GetMousePosition();
+
+
+    tempBlock.height=BLOCK_SIZE;
+    tempBlock.width=BLOCK_SIZE;
+
+    tempBlock.x = 25 * floor(mousePosition.x/25);
+    tempBlock.y = 25 * floor(mousePosition.y/25);
+
+
+    blocks.insert(blocks.end(),tempBlock);
+}
+
+
 int main(){
     InitWindow(WINDOW_WIDTH,WINDOW_HEIGHT,"Tetris");
     SetTargetFPS(60);
@@ -52,52 +96,17 @@ int main(){
         BeginDrawing();
 
         if(IsMouseButtonPressed(MOUSE_BUTTON_LEFT)){
-
-            Rectangle tempBlock;
-            Vector2 mousePosition = GetMousePosition();
-
-
-            tempBlock.height=BLOCK_SIZE;
-            tempBlock.width=BLOCK_SIZE;
-
-            tempBlock.x = 25 * floor(mousePosition.x/25);
-            tempBlock.y = 25 * floor(mousePosition.y/25);
-
-
-
-            blocks.insert(blocks.end(),tempBlock);
+            spawnBlock(blocks);
         }
 
 
         if(blockFallDelay()){
-            for(Rectangle& block:blocks){
-                if(((block.y+25)<=(top[block.x]))){
-                    std::cout<<"A"<<std::endl;
-                    block.y+=25;
-                } 
-                else{
-                    std::cout<<"B"<<std::endl;
-                    top[block.x]=block.y-25;                
-                }
-            }
+            updateBlocksTop(top,blocks);
         }
 
 
+        visualieGrid(visualise);
 
-        if(visualiseGrid)
-        {        
-            for(int x=0;x<WINDOW_WIDTH;x=x+BLOCK_SIZE){
-                for(int y=0;y<WINDOW_HEIGHT;y+=BLOCK_SIZE){
-                    Rectangle cell = {
-                        (float)x,
-                        (float)y,
-                        (float)BLOCK_SIZE,
-                        (float)BLOCK_SIZE,   // since we are drawing a square width and height are the same i.e. BLOCK_SIZE
-                    };
-                    DrawRectangleLinesEx(cell,0.5,whiteMask);
-                }
-            }
-        }
 
         Draw(blocks);
 
